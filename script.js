8d3c5a8c698a67f84c644f53ccae7161ac2c25b3
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-07-21 19:47:16 -0500

    created 16X16 grid

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let containerDiv = document.getElementById("container")
+let gridDiv;
 
 
-
-for(let i = 0; i < 16; i++){
-    let gridDiv = document.createElement("div")
+for(let i = 0; i < 256; i++){
+    gridDiv = document.createElement("div")
+    gridDiv.className = "gridDiv"
     containerDiv.insertAdjacentElement('beforeend',gridDiv)
 }
+
