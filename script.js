4430257d9d867a2cf7631a252c94592e940b6dc6
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-07-21 22:59:21 -0500

    added a button to clear canvas

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 let containerDiv = document.getElementById("container")
 let gridDiv;
-
+let clearGridBtn = document.getElementById("clearGridBtn")
+let numberOfGrids = prompt("Enter number of squares per side: ")
 
 for(let i = 0; i < 256; i++){
     gridDiv = document.createElement("div")
     gridDiv.className = "gridDiv"
     containerDiv.insertAdjacentElement('beforeend',gridDiv)
     gridDiv.addEventListener("mouseover", function(e){
-        e.target.style.backgroundColor = "pink"
-    })
+        e.target.style.backgroundColor = "black"
+    })   
 }
 
+
+clearGridBtn.addEventListener("click", ()=>{
+    gridDiv = document.getElementsByClassName("gridDiv")
+    for (let i = 0; i < gridDiv.length; i++){
+        gridDiv[i].removeAttribute("style")
+    }
+  
+
+})
