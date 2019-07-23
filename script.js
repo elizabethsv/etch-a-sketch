f29648a7e301ee4443edb4d8f78e4a6062f86519
Elizabeth Vasquez <vasquezelizabeth03@gmail.com>
2019-07-22 19:05:24 -0500

    btn to create new canvas with user's chosen # of grids

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 let containerDiv = document.getElementById("container")
 let gridDiv;
 let clearGridBtn = document.getElementById("clearGridBtn")
-let numberOfGrids = prompt("Enter number of squares per side: ")
+let newGridBtn = document.getElementById("newGridBtn")
+let numberOfGrids = 0
 
-for(let i = 0; i < 256; i++){
-    gridDiv = document.createElement("div")
-    gridDiv.className = "gridDiv"
-    containerDiv.insertAdjacentElement('beforeend',gridDiv)
-    gridDiv.addEventListener("mouseover", function(e){
-        e.target.style.backgroundColor = "black"
-    })   
-}
 
+function makeGrid(number){
+    for(let i = 0; i < (number**2); i++){
+        gridDiv = document.createElement("div")
+        gridDiv.className = "gridDiv"
+        containerDiv.style.gridTemplateColumns = `repeat(${number}, 20px)`
+        containerDiv.style.gridTemplateRows = `repeat(${number}, 20px)`
+        containerDiv.insertAdjacentElement('beforeend',gridDiv)
+        gridDiv.addEventListener("mouseover", function(e){
+            e.target.style.backgroundColor = "black"
+        })   
+     }
+ }
+ 
+
+makeGrid(16)
 
 clearGridBtn.addEventListener("click", ()=>{
     gridDiv = document.getElementsByClassName("gridDiv")
     for (let i = 0; i < gridDiv.length; i++){
         gridDiv[i].removeAttribute("style")
     }
-  
+})
 
+newGridBtn.addEventListener("click", ()=>{
+    
+    let gridDivsToRemove = document.querySelectorAll(".gridDiv")
+    for(let i = 0; i< gridDivsToRemove.length; i++){
+        gridDivsToRemove[i].remove()
+    }
+    numberOfGrids = prompt("Grid size: ");
+    Number(numberOfGrids)
+    makeGrid(numberOfGrids);
 })
